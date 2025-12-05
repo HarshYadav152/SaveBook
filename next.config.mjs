a5a044aecb17d4fd15e49d0ae776e3f579552570
HarshYadav152 <harshyadav152@outlook.com>
2025-12-05 12:38:25 +0530

    delete moved files

--- next.config.mjs
+++
@@ -1,9 +0,0 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-    eslint: {
-    // Completely disable ESLint during builds
-    ignoreDuringBuilds: true,
-  },
-};
-
-export default nextConfig;
