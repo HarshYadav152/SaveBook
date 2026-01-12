68c36a902049ee655ece02e22d612267780eecf0
Harsh Yadav <161936085+HarshYadav152@users.noreply.github.com>
2026-01-12 11:51:24 +0530

    one remaining import path update

--- savebook/app/api/auth/update-profile/route.js
+++ savebook/app/api/auth/update-profile/route.js
@@ -1,6 +1,6 @@
 import dbConnect from "@/lib/db/mongodb";
 import User from "@/lib/models/User";
-import { verifyJwtToken } from "@/lib/utils/jwt";
+import { verifyJwtToken } from "@/lib/utils/jwtAuth";
 import mongoose from "mongoose";
 import { NextResponse } from "next/server";
 
